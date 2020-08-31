f04be6a739aec072570c7417fceb1cbff02c9cf1
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-08-31 20:02:33 -0300

    11

---
+++ w03e11.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleOutputCP(1252);
+	
+	int x, y;
+	
+	printf("Escreva dois números decimais: ");
+	scanf("%d%d", &x, &y);
+	
+	if (x > 0 && y > 0) {
+		printf("O ponto P está no 1º quadrante.");
+	} else if (x < 0 && y > 0) {
+		printf("O ponto P está no 2º quadrante.");
+	} else if (x < 0 && y < 0) {
+		printf("O ponto P está no 3º quadrante.");
+	} else if (x > 0 && y < 0) {
+		printf("O ponto P está no 4º quadrante.");
+	} else if (x == 0 && y > 0) {
+		printf("O ponto P está entre o 1º e 2º quadrante.");
+	} else if (x == 0 && y < 0) {
+		printf("O ponto P está entre o 3º e 4º quadrante.");
+	} else if (y == 0 && x < 0) {
+		printf("O ponto P está entre o 2º e 3º quadrante.");
+	} else if (y == 0 && x > 0) {
+		printf("O ponto P está entre o 1º e 4º quadrante.");
+	} else {
+		printf("O ponto P está no centro do plano cartesiano.");
+	}
+	
+	return 1;
+}
