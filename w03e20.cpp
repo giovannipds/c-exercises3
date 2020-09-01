79813a6918495346c957b2b8d4846ed0d6937f85
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-09-01 02:26:17 -0300

    Giving up

--- w03e20.cpp
+++ w03e20.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
+#include <math.h>
 
 int getHours(char *msg)
 {
@@ -33,12 +34,33 @@ int main()
 {
 	SetConsoleOutputCP(1252);
 	
-	int ihours, imins, fhours, fmins;
+	int ihours, imins, fhours, fmins, dhours, dmins;
+	float price;
 	
 	ihours = getHours("Qual é o horário de entrada? ");
 	imins = getMinutes("Qual é o minuto de entrada? ");
 	fhours = getHours("Qual foi o horário de sáida? ");
 	fmins = getMinutes("Qual foi o minuto de saída? ");
+	
+	dhours = fhours >= ihours ? fhours - ihours : 24 - ihours + fhours;
+	dmins = abs(fmins - imins);
+	
+	printf("O valor a pagar é: ");
+	if (dhours >= 17) {
+		printf("Preço único R$ 50,00.");
+	} else if (dhours >= 3 && dhours <= 16) {
+		price = 12;
+		price += (dhours - 2) * 2.5;
+		printf("R$ %.2f.", price);
+	} else if (dhours <= 2 && dhours > 1) {
+		printf("R$ 12,00.");
+	} else if (dhours <= 1 && dhours != 0) {
+		printf("R$ 7,00.");
+	} else if (dmins <= 30 && dmins > 5) {
+		printf("R$ 4,00.");
+	} else {
+		printf("Isento.");
+	}
 	
 	return 1;
 }
